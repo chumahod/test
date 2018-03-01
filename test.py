f28c0a7cceff9f4b8c432b59fe35f7c458c3050f
chumahod <Dellippon001>
2018-03-01 20:32:47 +0500

    add utility screenshot

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import pyautogui as p
 import os
 import time
 import logging
+
+t_wait = 5
 # изменить  логи#
 logging.basicConfig(
     level=logging.INFO,
@@ -36,12 +38,19 @@ def onMap():
 def onCastle():
     # переключаемся в замок
 
+def shieldEnable():
+    if !(p.locateOnScreen("shield.png", region(750, 160, 0, 0)), grayscale=True):
+        # запускаем щит
+    else:
+        loggin.info("shield enable")
+
 if __name__ == '__main__':
     time.sleep(2)
     for i in range(2):
         shelter()
-        print("wait 60 min")
-        time.sleep(5)
+        print("wait ", t_wait, " sec")
+        print("wait ", t_wait/60, " min")
+        time.sleep(t_wait)
     print("well done")
 
 
---
+++ util/screenshot.py
@@ -0,0 +1,5 @@
+import pyautogui as p
+import os
+import time
+
+p.screenshot("img.png", region(750, 160, 810, 220))
